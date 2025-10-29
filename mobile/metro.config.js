4250b7daf0abba5a4dd4b0f87c1254e018d1192a
Christopher Obocha <chris@runwae.io>
2025-10-29 08:38:27 +0000

    fixed metro

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,12 +4,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-// Explicitly merge the transformer for NativeWind
-module.exports = withNativeWind({
-  ...defaultConfig,
-  transformer: {
-    ...defaultConfig.transformer,
-    // required by NativeWind
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-}, { input: './global.css' });
+module.exports = withNativeWind(defaultConfig, { input: './global.css' });

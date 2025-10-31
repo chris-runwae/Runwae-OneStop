85b98f52051246cca463fa932bcebb3c19e7695d
Christopher Obocha <chris@runwae.io>
2025-10-31 13:48:31 +0000

    WIP - Screen Container

---
+++ mobile/components/containers/ScreenContainer.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+  useColorScheme,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { BellIcon } from "lucide-react-native";
+import { Colors } from "@/constants";
+
+type ScreenContainerProps = {
+  children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  header?: {
+    rightComponent?: React.ReactNode;
+    title?: string;
+    leftComponent?: React.ReactNode;
+  };
+};
+
+const ScreenContainer = ({ children, style, header }: ScreenContainerProps) => {
+  const colorScheme = useColorScheme() ?? "light";
+  const insets = useSafeAreaInsets();
+
+  const styles = StyleSheet.create({
+    //Container
+    container: {
+      flex: 1,
+      backgroundColor: Colors[colorScheme].white,
+      paddingTop: insets.top + 10,
+    },
+
+    //Header
+    header: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingHorizontal: 16,
+      paddingBottom: 12,
+    },
+    rightComponent: {
+      flex: 1,
+      alignItems: "flex-end",
+    },
+    leftComponent: {
+      flex: 1,
+      alignItems: "flex-start",
+    },
+    headerTitle: {
+      color: Colors[colorScheme].headerText,
+      fontFamily: "Space Grotesk",
+      fontWeight: "500",
+      fontSize: 25,
+      lineHeight: 30,
+      letterSpacing: 0,
+    },
+    svgContainer: {
+      borderWidth: 1,
+      borderColor: Colors[colorScheme].headerGrey,
+      height: 50,
+      aspectRatio: 1,
+      borderRadius: 25,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+
+    //Content
+    content: {
+      flex: 1,
+      // paddingHorizontal: 15,
+      backgroundColor: Colors[colorScheme].backgroundColor,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.leftComponent}>
+          {header?.leftComponent ? (
+            header?.leftComponent
+          ) : (
+            <Text style={styles.headerTitle}>{header?.title ?? "Home"}</Text>
+          )}
+        </View>
+
+        <View style={styles.rightComponent}>
+          {header?.rightComponent ? (
+            header?.rightComponent
+          ) : (
+            <View style={styles.svgContainer}>
+              <BellIcon size={20} color={Colors[colorScheme].headerIcon} />
+            </View>
+          )}
+        </View>
+      </View>
+
+      <View style={styles.content}>{children}</View>
+    </View>
+  );
+};
+
+export default ScreenContainer;

b6b23806821856c13c7643798a84fde5277a2727
Christopher Obocha <chris@runwae.io>
2025-10-31 16:07:32 +0000

    fixed UI

--- mobile/components/headers/ExpandLink.tsx
+++ mobile/components/headers/ExpandLink.tsx
@@ -1,8 +1,14 @@
-import { Pressable, StyleSheet, Text, View, useColorScheme } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from "react-native";
 import React from "react";
 
-import { RelativePathString, router } from 'expo-router';
-import { MoveRight } from 'lucide-react-native';
+import { RelativePathString, router } from "expo-router";
+import { MoveRight } from "lucide-react-native";
 import { Colors } from "@/constants/theme";
 
 export interface ExpandLinkProps {
@@ -10,17 +16,38 @@ export interface ExpandLinkProps {
   linkTo?: RelativePathString;
 }
 
-const ExpandLink = ({
-  linkText,
-  linkTo,
-}: ExpandLinkProps) => {
-  const colorScheme = useColorScheme() ?? 'light';
+const ExpandLink = ({ linkText, linkTo }: ExpandLinkProps) => {
+  const colorScheme = useColorScheme() ?? "light";
+  const colors = Colors[colorScheme];
+
+  const styles = StyleSheet.create({
+    linkContainer: {
+      backgroundColor: colors.primaryColors.background,
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 6,
+      paddingVertical: 4,
+      paddingHorizontal: 12,
+      borderRadius: 100,
+      borderWidth: 1,
+      borderColor: colors.primaryColors.border,
+    },
+
+    linkText: {
+      color: colors.primaryColors.default,
+      fontSize: 11,
+      fontWeight: "400",
+      fontFamily: "DM Sans",
+      lineHeight: 16.5,
+      letterSpacing: 0,
+    },
+  });
 
   if (!linkTo) {
     return (
       <View style={styles.linkContainer}>
         <Text style={styles.linkText}>{linkText}</Text>
-        <MoveRight size={20} color={Colors[colorScheme].pink500} />
+        <MoveRight size={12} color={colors.primaryColors.default} />
       </View>
     );
   }
@@ -28,29 +55,12 @@ const ExpandLink = ({
   return (
     <Pressable
       onPress={() => router.push(linkTo!)}
-      style={styles.linkContainer}>
-      <Text style={[styles.linkText, { color: Colors[colorScheme].pink500 }]}>{linkText}</Text>
-      <MoveRight size={20} color={Colors[colorScheme].pink500} />
+      style={styles.linkContainer}
+    >
+      <Text style={styles.linkText}>{linkText}</Text>
+      <MoveRight size={12} color={colors.primaryColors.default} />
     </Pressable>
   );
 };
 
-
 export default ExpandLink;
-
-
-const styles = StyleSheet.create({
-  linkContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 6,
-  },
-
-  linkText: {
-    fontSize: 16,
-    fontWeight: '400',
-    fontFamily: 'DM Sans',
-    lineHeight: 24,
-    letterSpacing: 0,
-  },
-});

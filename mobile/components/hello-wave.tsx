832acd46096cee7e981077021a99e7bc7d313cf0
Christopher Obocha <chris@runwae.io>
2025-10-31 15:27:27 +0000

    modified hello wave

--- mobile/components/hello-wave.tsx
+++ mobile/components/hello-wave.tsx
@@ -1,18 +1,19 @@
-import Animated from 'react-native-reanimated';
+import Animated from "react-native-reanimated";
 
-export function HelloWave() {
+export function HelloWave({ size = 28 }: { size?: number }) {
   return (
     <Animated.Text
       style={{
-        fontSize: 28,
-        lineHeight: 32,
-        marginTop: -6,
+        fontSize: size,
+        lineHeight: size + 4,
+        marginTop: -size / 4,
         animationName: {
-          '50%': { transform: [{ rotate: '25deg' }] },
+          "50%": { transform: [{ rotate: "25deg" }] },
         },
         animationIterationCount: 4,
-        animationDuration: '300ms',
-      }}>
+        animationDuration: "300ms",
+      }}
+    >
       👋
     </Animated.Text>
   );

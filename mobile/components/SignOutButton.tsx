f0189ce0eae91fdc2ccd991abfa53b2a94f19247
Christopher Obocha <chris@runwae.io>
2025-11-03 07:21:18 +0000

    WIP SignOut

--- mobile/components/SignOutButton.tsx
+++ mobile/components/SignOutButton.tsx
@@ -1,28 +1,24 @@
-import { useClerk } from '@clerk/clerk-expo'
-import { useRouter } from 'expo-router'
-import { Text, TouchableOpacity } from 'react-native'
+import { useClerk } from "@clerk/clerk-expo";
+import { useRouter, RelativePathString } from "expo-router";
+import { Button } from "react-native";
 
 export const SignOutButton = () => {
   // Use `useClerk()` to access the `signOut()` function
-  const { signOut } = useClerk()
-  const router = useRouter()
+  const { signOut } = useClerk();
+  const router = useRouter();
 
   const handleSignOut = async () => {
     try {
-      await signOut()
-      console.log('Signed out successfully');
+      await signOut();
+      console.log("Signed out successfully");
       // Redirect to your desired page
-      router.replace('/(auth)/sign-in')
+      router.replace("/(auth)/sign-in" as RelativePathString);
     } catch (err) {
       // See https://clerk.com/docs/guides/development/custom-flows/error-handling
       // for more info on error handling
-      console.error(JSON.stringify(err, null, 2))
+      console.error(JSON.stringify(err, null, 2));
     }
-  }
+  };
 
-  return (
-    <TouchableOpacity onPress={handleSignOut}>
-      <Text>Sign out</Text>
-    </TouchableOpacity>
-  )
-}
+  return <Button title="Sign out" onPress={handleSignOut} />;
+};

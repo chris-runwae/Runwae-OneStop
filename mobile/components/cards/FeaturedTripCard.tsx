17522755a1696a71030fe66d11570480b0a34168
Christopher Obocha <chris@runwae.io>
2025-11-03 18:46:17 +0000

    default image

--- mobile/components/cards/FeaturedTripCard.tsx
+++ mobile/components/cards/FeaturedTripCard.tsx
@@ -84,10 +84,7 @@ const FeaturedItemsCard = ({ data }: FeaturedItemsCardProps) => {
 
   return (
     <ImageBackground
-      // source={image}
-      source={{
-        uri: "https://images.unsplash.com/photo-1505843513577-22bb7d21e455?auto=format&fit=crop&w=1200&q=80",
-      }}
+      source={image}
       style={styles.imageContainer}
       contentFit="cover"
     >

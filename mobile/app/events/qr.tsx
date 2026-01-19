f70260052c5c6d093ae1457251c4d0fb9aec79e6
Christopher Obocha <chris@runwae.io>
2026-01-19 08:58:14 +0000

    fixes WIP

--- mobile/app/events/qr.tsx
+++ mobile/app/events/qr.tsx
@@ -1,7 +1,5 @@
-// app/events/qr.tsx
+import EventQRModal from '@/screens/events/EventQRModal';
 
-// import EventQRModal from '@/screens/events/EventQRModal';
+export { default as unstable_settings } from '@/config/modal-settings';
 
-// export { default as unstable_settings } from '@/config/modal-settings';
-
-// export default EventQRModal;
+export default EventQRModal;

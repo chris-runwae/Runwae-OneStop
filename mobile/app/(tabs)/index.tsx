fdd80272185b6dc28b9114e750067ffa502bb8d5
iamnathan-dev <nathancodes05@gmail.com>
2026-03-24 00:36:35 +0100

    feat: home - explore page update

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
+import AddOnsForYou from "@/components/home/AddOnsForYou";
 import HomeHeader from "@/components/home/HomeHeader";
 import ItineraryForYou from "@/components/home/IteneryForYou";
-import AddOnsForYou from "@/components/home/AddOnsForYou";
 import UpcomingTrips from "@/components/home/UpcomingTrips";
 import AppSafeAreaView from "@/components/ui/AppSafeAreaView";
 import WelcomeModal from "@/components/WelcomeModal";
@@ -12,13 +12,18 @@ import {
 } from "@/constants/home.constant";
 import { useAuth } from "@/context/AuthContext";
 import { useTheme } from "@react-navigation/native";
-import React, { useState, useCallback } from "react";
-import { ScrollView, RefreshControl } from "react-native";
+import React, { useCallback, useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
 
 import DestinationsForYou from "@/components/home/DestinationsForYou";
 
 export default function HomeScreen() {
-  const { showWelcomeModal, setShowWelcomeModal, user, isLoading: authLoading } = useAuth();
+  const {
+    showWelcomeModal,
+    setShowWelcomeModal,
+    user,
+    isLoading: authLoading,
+  } = useAuth();
   const { dark } = useTheme();
 
   const [refreshing, setRefreshing] = useState(false);
@@ -38,7 +43,6 @@ export default function HomeScreen() {
       setRefreshing(false);
     }, 1500);
   }, []);
-
 
   return (
     <AppSafeAreaView edges={["top"]}>
@@ -60,7 +64,6 @@ export default function HomeScreen() {
         <AddOnsForYou data={ADD_ONS_FOR_YOU} loading={loading} />
         <DestinationsForYou data={DESTINATIONS_FOR_YOU} loading={loading} />
       </ScrollView>
-
 
       <WelcomeModal
         visible={showWelcomeModal}

f2c89272e09d0c0a793c68ac0953e5e200671524
Christopher Obocha <christopherobocha@gmail.com>
2026-03-24 11:08:19 +0000

    WIP - add polls

--- mobile/app/(tabs)/(trips)/_layout.tsx
+++ mobile/app/(tabs)/(trips)/_layout.tsx
@@ -1,11 +1,19 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack } from 'expo-router';
+import React from 'react';
 
 export default function TripsLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="[id]/index" />
+      <Stack.Screen
+        name="[id]/add-poll"
+        options={{
+          headerShown: false,
+          presentation: 'formSheet',
+          sheetAllowedDetents: [0.9, 1],
+        }}
+      />
     </Stack>
   );
 }

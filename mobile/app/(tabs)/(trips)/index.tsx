520e66339def4a664e12665fc5d80b32ad8941d1
Christopher Obocha <christopherobocha@gmail.com>
2026-03-26 11:18:44 +0000

    adding trips ui

--- mobile/app/(tabs)/(trips)/index.tsx
+++ mobile/app/(tabs)/(trips)/index.tsx
@@ -1,4 +1,3 @@
 import TripsIndexScreen from '@/screens/trips/TripsIndexScreen';
-import React from 'react';
 
 export default TripsIndexScreen;
--- mobile/components/home/TripCard.tsx
+++ mobile/components/home/TripCard.tsx
@@ -1,36 +1,34 @@
-import { Trip } from "@/constants/home.constant";
-
-import React from "react";
-import { ImageBackground, Pressable, Text, View } from "react-native";
-import AvatarGroup from "./AvatarGroup";
+import React from 'react';
+import { ImageBackground, Pressable, Text, View } from 'react-native';
+// import AvatarGroup from "./AvatarGroup";
+import { TripWithEverything } from '@/hooks/useTripActions';
+import { AvatarGroup } from '../containers/AvatarGroup';
 
 interface TripCardProps {
-  trip: Trip;
+  trip: TripWithEverything;
   fullWidth?: boolean;
 }
 
 const TripCard = ({ trip, fullWidth = false }: TripCardProps) => {
   return (
     <Pressable
-      className="rounded-2xl overflow-hidden"
+      className="overflow-hidden rounded-2xl"
       onPress={() => {}}
-      style={{ width: fullWidth ? "100%" : 360, height: 210 }}
-    >
+      style={{ width: fullWidth ? '100%' : 360, height: 210 }}>
       <ImageBackground
-        source={{ uri: trip.image }}
+        source={{ uri: trip.cover_image_url ?? undefined }}
         className="flex-1"
-        resizeMode="cover"
-      >
+        resizeMode="cover">
         <View
           className="flex-1 justify-between p-3"
-          style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
-        >
+          style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}>
           {/* Role badge */}
           <View className="flex-row justify-end">
-            <View className="bg-[#000000A6] dark:bg-dark-seconndary rounded-full px-3 py-1">
+            <View className="rounded-full bg-[#000000A6] px-3 py-1 dark:bg-dark-seconndary">
               <Text className="text-xs font-semibold text-white">
-                {trip.role}
+                {trip.trip_details?.visibility}
               </Text>
+              {/* TODO: Add role */}
             </View>
           </View>
 
@@ -40,25 +38,21 @@ const TripCard = ({ trip, fullWidth = false }: TripCardProps) => {
               <View className="flex-1">
                 <View className="flex-row items-center justify-between">
                   <Text
-                    className="text-white text-lg font-bold"
-                    style={{ fontFamily: "BricolageGrotesque-ExtraBold" }}
-                  >
-                    {trip.title}
+                    className="text-lg font-bold text-white"
+                    style={{ fontFamily: 'BricolageGrotesque-ExtraBold' }}>
+                    {trip.name}
                   </Text>
 
-                  <AvatarGroup
-                    members={trip.members}
-                    extraMembers={trip.extraMembers}
-                  />
+                  <AvatarGroup members={trip.group_members || []} />
                 </View>
-                <Text className="text-white text-sm mt-0.5">
-                  📍 {trip.location}
+                <Text className="mt-0.5 text-sm text-white">
+                  📍 {trip.destination_label}
                 </Text>
               </View>
             </View>
 
-            <Text className="text-white text-sm mt-5">
-              {trip.dateRange} | {trip.duration} | {trip.items} items
+            <Text className="mt-5 text-sm text-white">
+              {trip.trip_details?.start_date} - {trip.trip_details?.end_date}
             </Text>
           </View>
         </View>
